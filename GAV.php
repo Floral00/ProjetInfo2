41d061d965d5ce7613252aa9fe56dcd313fa1c60
Floral00 <49778405+Floral00@users.noreply.github.com>
2020-01-29 10:14:27 +0100

    Create GAV.php

---
+++ GAV.php
@@ -0,0 +1,122 @@
+<?php
+include("Initialiser.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Modern Business - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">King Of Beer</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="MAC.php">MAC Carthy</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="GAV.php">Gavroche</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ARC.php">Arcade</a>
+          </li>
+		   <li class="nav-item">
+            <a class="nav-link" href="QUI.php">Quincaillerie</a>
+          </li>
+		   <li class="nav-item">
+            <a class="nav-link" href="BER.php">Berthom</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+      <br/><br/>
+    <!-- Page Heading/Breadcrumbs -->
+    <h1 class="mt-4 mb-3">Le Gavroche
+      <small>à Nancy</small>
+    </h1>
+
+    <!-- Intro Content -->
+    <div class="row">
+      <div class="col-lg-6">
+        <img class="img-fluid rounded mb-4" src="http://placehold.it/750x450" alt="">
+      </div>
+      <div class="col-lg-6">
+        <h2>Le Gavroche</h2>
+        <p>Profitez d'une salle de Billard au sous-sol du MAC-Carthy pour des soirées entres amis. Bonnes bières pas cher</p>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Team Members -->
+    <h2>Nos bières</h2>
+
+    <div class="row">
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100 text-center">
+          <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Chouffe</h4>
+            <h6 class="card-subtitle mb-2 text-muted">Blonde</h6>
+            <p class="card-text">Bière blonde assez forte 8% mais douce en bouche</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100 text-center">
+          <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Chouffe</h4>
+            <h6 class="card-subtitle mb-2 text-muted">Blonde</h6>
+            <p class="card-text">Bière blonde assez forte 8% mais douce en bouche</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100 text-center">
+          <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Chouffe</h4>
+            <h6 class="card-subtitle mb-2 text-muted">Blonde</h6>
+            <p class="card-text">Bière blonde assez forte 8% mais douce en bouche</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
